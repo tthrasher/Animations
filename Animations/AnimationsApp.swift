0e74c2b8481f31ae19ae83730b63b34c1ed63722
tthrasher <tthrasher@gmail.com>
2021-04-22 15:27:39 -0400

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Terry Thrasher on 2021-04-22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
