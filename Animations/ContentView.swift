d0c00374ed3c3d96938e4ff27a030429d7929c4a
tthrasher <tthrasher@gmail.com>
2021-04-26 23:04:01 -0400

    Initial commit message

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,9 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isShowingRed = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Button("Tap it") {
+                // do nothing yet
+            }
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 200, height: 200)
+            }
+        }
+        
+        /* This is a chunk of animation content that I didn't want to delete before working on gestures.
+        print(animationAmount)
+        
+        return VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(
+                Animation.easeInOut(duration: 1)
+                    .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
+            
+            Spacer()
+            
+            Button("Tap it") {
+                    self.enabled.toggle()
+            }
+            .frame(width: 200, height: 200)
+            .background(enabled ? Color.red : Color.blue)
+            .animation(.default)
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+            .animation(.interpolatingSpring(stiffness: 10, damping: 1))
+        }
+         */
     }
 }
 
