79460d2f8602ee003efda179b0982c18cfae4c82
tthrasher <tthrasher@gmail.com>
2021-04-28 17:36:03 -0400

    Asymmetric animation demo added

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -13,38 +13,17 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Button("Tap it") {
-                // do nothing yet
-            }
-            
+                withAnimation {
+                    self.isShowingRed.toggle()            }
+                }
+                
             if isShowingRed {
                 Rectangle()
                     .fill(Color.red)
                     .frame(width: 200, height: 200)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
             }
         }
-        
-        /* This is a chunk of animation content that I didn't want to delete before working on gestures.
-        print(animationAmount)
-        
-        return VStack {
-            Stepper("Scale amount", value: $animationAmount.animation(
-                Animation.easeInOut(duration: 1)
-                    .repeatCount(3, autoreverses: true)
-            ), in: 1...10)
-            
-            Spacer()
-            
-            Button("Tap it") {
-                    self.enabled.toggle()
-            }
-            .frame(width: 200, height: 200)
-            .background(enabled ? Color.red : Color.blue)
-            .animation(.default)
-            .foregroundColor(.white)
-            .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
-            .animation(.interpolatingSpring(stiffness: 10, damping: 1))
-        }
-         */
     }
 }
 
